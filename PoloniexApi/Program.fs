0b1070410f4f635d5c67b61a026292d077df94ee
Allison Wong <wonga10@gmail.com>
2017-09-04 16:43:39 -0700

    Add returnOpenOrders

--- PoloniexApi/Program.fs
+++ PoloniexApi/Program.fs
@@ -7,14 +7,17 @@ open Client.TradingApi
 
 [<EntryPoint>]
 let main argv = 
+    let BTC_XMR = "BTC_XMR"
     let keys = File.ReadAllLines(@"..\..\Keys.txt")
     let apiKey, secret = keys.[0], keys.[1]
     let tradingClient = Client.TradingApi.Client(apiKey, secret)
     let publicClient = Client.PublicApi.Client()
     //let result = tradingClient.TransferBalance("BTC", 0.0005m, Margin, Exchange).Result
-    let result = tradingClient.Buy("BTC_XMR", 0.02500001m, 0.1m).Result.Result()
-    let orderNumber = result.orderNumber
-    let result = tradingClient.CancelOrder(196735019163L).Result
+    let result = tradingClient.Buy(BTC_XMR, 0.02300001m, 0.1m).Result.Result()
+    let openOrder = tradingClient.ReturnOpenOrders(BTC_XMR).Result.Result()
+    let tets = openOrder |> Seq.head
+    printfn "%d amt %M" tets.orderNumber tets.amount
+    let result = tradingClient.CancelOrder(result.orderNumber).Result
     printfn "%s" (result.Content.ReadAsStringAsync().Result)
     Console.ReadKey() |> ignore
     ignore (Console.ReadKey())
